375a30cf6cd6d63f602304012c81d767a768e4dd
Peter Wong <peter.wong@deepcodify.com>
2023-09-21 16:22:46 +0800

    Impl record_new_ans and save_last_day_record func

--- daily.py
+++ daily.py
@@ -1,13 +1,41 @@
+import datetime
+import json
+
 import helpers
 
 
-def record_today_ans(ans: str) -> None:
-    with open("./wkldle_stat/today_ans.txt", "w") as f:
-        f.write(ans)
+def record_new_ans(ans: str) -> None:
+    content = {
+        "ans": ans,
+    }
+    json_content = json.dumps(content, indent=2)
+
+    with open("./wkldle_stat/today_ans.json", "w") as f:
+        f.write(json_content)
+
+    return
 
 
-# TODO: Impl
 def save_last_day_record() -> None:
+    today_date = datetime.date.today().strftime("%Y-%m-%d")
+
+    today_ans = {}
+    with open("./wkldle_stat/today_ans.json", "r") as f:
+        today_ans = json.load(f)
+
+    today_log = []
+    with open("./wkldle_stat/log.json", "r") as f:
+        today_log = json.load(f)
+
+    today_log.insert(
+        0,
+        today_ans,
+    )
+    json_log = json.dumps(today_log, indent=2)
+
+    with open(f"./wkldle_stat/prev_log/log.{today_date}.json", "w") as f:
+        f.write(json_log)
+
     return
 
 
@@ -17,12 +45,16 @@ def get_wkldle_readme_text() -> str:
     return readme_text
 
 
-# TODO: Save last day record
 # TODO: Update readme
 def main() -> None:
-    dictionary = helpers.get_local_dictionary()
+    save_last_day_record()
 
+    dictionary = helpers.get_local_dictionary()
     ans = helpers.pick_random_word(dictionary)
-    record_today_ans(ans)
+    record_new_ans(ans)
 
     return
+
+
+if __name__ == "__main__":
+    main()
