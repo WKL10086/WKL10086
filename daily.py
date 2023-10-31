95a9feccf78ec5c7f08af973dcbd9bb813e3d7cd
Peter Wong <peter.wong@deepcodify.com>
2023-10-31 16:21:34 +0800

    Impl get_wkldle_readme_text()

--- daily.py
+++ daily.py
@@ -44,16 +44,99 @@ def init_game_state() -> None:
         game_state = {chr(i): "not-guessed" for i in range(ord("A"), ord("Z") + 1)}
         json.dump(game_state, f)
 
+    return
+
 
 def init_log() -> None:
     with open("./wkldle_stat/log.json", "w") as f:
         json.dump([], f)
 
+    return
 
-# TODO: Impl
+
 def get_wkldle_readme_text() -> str:
-    readme_text = f""""""
-    return readme_text
+    style_text = helpers.get_style_readme_text()
+
+    readme_text = """\
+<div class="wrapper">
+  <div class="board">
+    <div class="row">
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+      <div class="item">!</div>
+    </div>
+  </div>
+
+  <br />
+
+  <div class="keyboard">
+    <div class="keyboard-row">
+      <div class="item not-guessed">Q</div>
+      <div class="item not-guessed">W</div>
+      <div class="item not-guessed">E</div>
+      <div class="item not-guessed">R</div>
+      <div class="item not-guessed">T</div>
+      <div class="item not-guessed">Y</div>
+      <div class="item not-guessed">U</div>
+      <div class="item not-guessed">I</div>
+      <div class="item not-guessed">O</div>
+      <div class="item not-guessed">P</div>
+    </div>
+    <br />
+    <div class="keyboard-row">
+      <div class="item not-guessed">A</div>
+      <div class="item not-guessed">S</div>
+      <div class="item not-guessed">D</div>
+      <div class="item not-guessed">F</div>
+      <div class="item not-guessed">G</div>
+      <div class="item not-guessed">H</div>
+      <div class="item not-guessed">J</div>
+      <div class="item not-guessed">K</div>
+      <div class="item not-guessed">L</div>
+    </div>
+    <br />
+    <div class="keyboard-row">
+      <div class="item not-guessed">Z</div>
+      <div class="item not-guessed">X</div>
+      <div class="item not-guessed">C</div>
+      <div class="item not-guessed">V</div>
+      <div class="item not-guessed">B</div>
+      <div class="item not-guessed">N</div>
+      <div class="item not-guessed">M</div>
+    </div>
+  </div>
+</div>
+
+"""
+
+    return style_text + readme_text
 
 
 def update_readme(readme_text: str) -> None:
