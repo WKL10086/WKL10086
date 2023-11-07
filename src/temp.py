12c55c5aa6c60f4c7f5ca76856830f7f668c5b71
Peter Wong <peter.wong@deepcodify.com>
2023-11-07 17:34:32 +0800

    Delete temp file

--- src/temp.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Create a dictionary with uppercase letter keys and empty string values
-game_state = {chr(i): "" for i in range(ord("A"), ord("Z") + 1)}
-
-# Write the dictionary to a JSON file
-with open("game_state.json", "w") as f:
-    json.dump(game_state, f)
