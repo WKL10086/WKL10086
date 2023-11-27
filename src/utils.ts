89d2ebf33df236a451a91dd4ce84ae934840c2c7
Peter Wong <peter.wong@deepcodify.com>
2023-11-27 17:05:14 +0800

    Add utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+export interface ENV {
+  mode: "DAILY" | "MAIN";
+  accessToken: string;
+  issueTitle?: string;
+  userName?: string;
+}
+
+export const getEnv = (): ENV => {
+  const mode = (process.env.MODE as "DAILY" | "MAIN") ?? "MAIN";
+  const accessToken = process.env.ACCESS_TOKEN ?? "";
+  const issueTitle = process.env.ISSUE_TITLE;
+  const userName = process.env.USER_NAME;
+
+  const env: ENV = {
+    mode,
+    accessToken,
+    issueTitle,
+    userName,
+  };
+
+  return env;
+};
