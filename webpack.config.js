085e56fff504029987e0b23492d783b528c57b11
Peter Wong <peter.wong@deepcodify.com>
2023-11-29 18:03:05 +0800

    Add clean-webpack-plugin to package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  target: "node",
   entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: {
+      // name: "wkldle-lib",
+      type: "umd",
+    },
+    // globalObject: "this",
+  },
+  devtool: "source-map",
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 4000,
-  },
+  plugins: [new CleanWebpackPlugin()],
 };
