8dd4cc1e48c7541e27c5fc32795af825eaae51da
Peter Wong <peter.wong@deepcodify.com>
2024-05-16 17:26:35 +0800

    chore: Update .gitignore and package.json, add tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+
+// import * as core from "@actions/core";
+import { createOrUpdateTextFile } from "@octokit/plugin-create-or-update-text-file";
+import { Octokit } from "octokit";
+
+const MyOctokit = Octokit.plugin(createOrUpdateTextFile).defaults({
+  userAgent: "WKL10086-README-Action",
+});
+
+const octokit = new MyOctokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const main = async () => {
+  const getReadme = await octokit
+    .request("GET /repos/{owner}/{repo}/contents/{path}", {
+      owner: "WKL10086",
+      repo: "WKL10086",
+      path: "README.md",
+    })
+    .catch((e: any) => {
+      console.error("Failed: ", e);
+      // core.setFailed("Failed: ", e.message);
+    });
+
+  // console.log("getReadme.data: ", getReadme.data);
+
+  const content = Buffer.from(getReadme.data.content, "base64").toString();
+  console.log("content: ", content);
+};
+
+main();
